88f9e9ebbd16753b36132744f238a32eaa76cd70
Hasan Mohsin <hasan@hasali.dev>
2023-12-02 15:44:48 +0000

    Avoid depending on xh

--- src/main.rs
+++ src/main.rs
@@ -25,15 +25,17 @@ fn init(day: u32) -> eyre::Result<()> {
     let input_url = format!("https://adventofcode.com/2023/day/{day}/input");
     let solution_path = PathBuf::from(format!("solutions/day_{day}"));
     let aoc_session = env::var("AOC_SESSION")?;
-    let aoc_cookie = format!("cookie: session={aoc_session}");
 
     if !solution_path.exists() {
         cmd!("cargo", "new", solution_path).run()?;
     }
 
-    cmd!("xh", input_url, aoc_cookie)
-        .stdout_path(format!("solutions/day_{day}/input.txt"))
-        .run()?;
+    let input = ureq::get(&input_url)
+        .set("Cookie", &format!("session={aoc_session}"))
+        .call()?
+        .into_string()?;
+
+    std::fs::write(format!("solutions/day_{day}/input.txt"), &input)?;
 
     Ok(())
 }
